6b990de71ae41a0a1a1d5581c7697d25b79f14ca
guru28272 <117896953+guru28272@users.noreply.github.com>
2023-03-03 14:17:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // your code here
+const form = document.querySelector(`form`);
+const nameEle = document.querySelector(`#name`);
+const yearEle = document.querySelector(`#year`);
+const btn = document.querySelector(`#button`);
+const h3 = document.querySelector(`h3`);
+
+form.addEventListener(`submit`, function (e) {
+  e.preventDefault();
+
+  const name = nameEle.value;
+  const year = yearEle.value;
+
+  if (name && !year) {
+    h3.textContent = `https://localhost:8080/?name=${name}`;
+  } else if (year && !name) {
+    h3.textContent = `https://localhost:8080/?year=${year}`;
+  } else if (name && year) {
+    h3.textContent = `https://localhost:8080/?name=${name}&year=${year}`;
+  } else if (!name && !year) {
+    h3.textContent = h3.innerText;
+  }
+});
